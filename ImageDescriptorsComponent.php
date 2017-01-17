2fa5de020c09e4f7adf9fd2c0ee8e52aa4586a8f
Gui Egas <guilhermeegas@gmail.com>
2017-01-16 23:48:02 -0200

    DCT and zigzag scan

--- ImageDescriptorsComponent.php
+++ ImageDescriptorsComponent.php
@@ -68,16 +68,44 @@ class ImageDescriptorsComponent extends Component
 
     }
 
-    protected function _zigzag()
+    protected function _zigzag($dct=[])
     {
         $zigzag = [1,2,9,17,10,3,4,11,18,25,33,26,19,12,5,6,13,29,27,34,41,49,42,35,28,21,14,7,8,15,22,29,36,43,50,57,58,51,44,37,30,23,16,24,31,38,45,52,59,60,53,46,39,32,49,47,54,61,62,55,48,56,63,64];
-        foreach ($zigzag as $key => $value) {
-            
+        $descritor = [];
+        foreach ($zigzag as $k => $i) {
+            $x = (int) (($i-1)/$this->_width);
+            $y = (int) (($i-1)%$this->_height);
+            $descritor[$k] = $dct[$x][$y];
         }
+        return $descritor;
     }
     protected function _DCT()
     {
-
+        
+        $dct = [];
+        $n = $this->_width;
+        $m = $this->_height;
+        $sqrt_1o2 = sqrt(1/2);
+        for ($k=0; $k < 3 ; $k++) { 
+            for ($u=0; $u<$n; $u++) {
+                for ($v=0; $v<$m; $v++) {
+                    $sum = 0;
+                    for ($i=0; $i<$n; $i++) {
+                        for ($j=0;$j<$m; $j++) {
+                            $m1 = ($i) ? 1: $sqrt_1o2;
+                            $m2 = ($j) ? 1: $sqrt_1o2;
+                            $cos1 = cos( ((pi()*$u) / (2*$n))*(2*$i+1));
+                            $cos2 = cos( ((pi()*$v) / (2*$m))*(2*$j+1));
+                            $f_ij = $this->_matrix[$i][$j][$k];
+                            $sum += $m1 * $m2 * $cos1 * $cos2 * $f_ij;
+                        }
+                    }
+                    $sum = sqrt(2/$n)*sqrt(2/$m)*$sum;
+                    $dct[$u][$v][$k] = $sum;
+                }
+            }
+        }
+        return $dct;
     }
     
     public function CLD($image_url=null)
@@ -87,6 +115,8 @@ class ImageDescriptorsComponent extends Component
         }
         $this->_partitioning();
         $this->_image2Matrix('ycbcr');
+        $dct = $this->_DCT();
+        $descritor = $this->_zigzag($dct);
     }
 
 }
